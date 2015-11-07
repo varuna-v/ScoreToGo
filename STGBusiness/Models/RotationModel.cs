9a17a0b2949514ce24133344561ed82968a6ac22
Varuna <varuna1510@gmail.com>
2015-11-07 14:11:14 +0000

    One team's rotation

---
+++ STGBusiness/Models/RotationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STGBusiness.Models
+{
+    public class RotationModel
+    {
+        public int[] ShirtNumbers {get; set;}
+
+        public RotationModel()
+        {
+            ShirtNumbers = new int[6];
+        }
+
+        public void Rotate()
+        {
+            int temporaryPosition = ShirtNumbers[0];
+            ShirtNumbers[0] = ShirtNumbers[1];
+            ShirtNumbers[1] = ShirtNumbers[2];
+            ShirtNumbers[2] = ShirtNumbers[3];
+            ShirtNumbers[3] = ShirtNumbers[4];
+            ShirtNumbers[4] = ShirtNumbers[5];
+            ShirtNumbers[5] = temporaryPosition;
+        }
+    }
+}
---
+++ ScoreToGo/Controllers/RotationController.cs
@@ -0,0 +1,27 @@
+﻿using STGBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScoreToGo.Controllers
+{
+    public class RotationController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new RotationModel()
+            {
+                ShirtNumbers = new int[] { 5, 12, 6, 7, 10, 1 }
+            };
+            return View(model);
+        }
+        
+        public ActionResult Rotate(RotationModel model)
+        {
+            model.Rotate();
+            return View("Index", model);
+        }
+    }
+}
